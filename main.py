18d84c773d6ecfa3594083025dbbad86dc120c87
zmiyzmoy <raselmik@gmail.com>
2021-04-29 15:08:11 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from pydub import *
+
+three_seconds = 3 * 1000
+
+song = AudioSegment.from_wav('eldar.wav')
+
+first_3_seconds = song[:three_seconds]
+
+first_3_seconds.export('R:/pfile.wav', format='wav')
